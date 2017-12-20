f491363c875cbbd2761aaca4a3709e8f56977ed2
FTdiscovery <gordonchi11@gmail.com>
2017-12-20 00:23:26 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+package ekalGO;
+
+/*
+ * Here we begin the exploration of EKAL - evste'na kalpak yo'nuron eyeshkomyod, a concept of Dutrum Mesnovich, 2009
+ * Developed in theory by Mesnovich at University of Ralyas, Piray.
+ * 
+ * This project starts on December 20, 2017, and focuses on the creation of a computer that learns the evaluation
+ * and methods to play the game of Go using a database of master's games, a deep artificial neural network, 
+ * Monte Carlo Tree Search to evaluate and update playout values for familiar positions and efficiency, 
+ * then reinforcement learning 'tournaments' between separately trained neural networks with different parameters to determine
+ * the best moves.
+ * 
+ * After a certain time of training, the computer will choose the best engine out of the created ones and pick that to compete against a human.
+ * The Neural Network is designed so that the synapses can be saved on a computer.
+ */
+
+public class Main {
+
+}
