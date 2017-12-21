843af7429d0b631b84e988b2cbc213244f8443d1
FTdiscovery <gordonchi11@gmail.com>
2017-12-21 03:51:18 -0500

    Update Board.java
    Start to work on capture mechanism.

--- Board.java
+++ Board.java
@@ -1,5 +1,7 @@
 package ekalGO;
 
+import java.util.Arrays;
+
 public class Board {
 
 	/*
@@ -8,12 +10,14 @@ public class Board {
 	 */
 	
 	double[][][] GO_BOARD;
+	int[] CAPTURES;
 	String[][] PRINTED;
 	String ALPHABET = "ABCDEFGHJKLMNOPQRST";
 			
 	public Board() {
 		GO_BOARD = new double[19][19][2];
 		PRINTED = new String[19][19];
+		CAPTURES = new int[2];
 	}
 	
 	public String output(double[] a) {
@@ -42,12 +46,70 @@ public class Board {
 		GO_BOARD[directory[1]][directory[0]][turn]=1;
 	}
 	
+	public double[] BoardToState() {
+		double[] output = new double[722];
+		for (int i = 0;i<19;i++) {
+			for (int j = 0;j<19;j++) {
+				output[38*i+2*j] = GO_BOARD[i][j][0];
+				output[38*i+2*j+1] = GO_BOARD[i][j][1];
+			}
+		}
+		return output;
+	}
+	
+	//Set the rules of Go, Captures for single stones done.
+	public void updateGoCaptures() {
+		for (int i = 0;i<19;i++) {
+			for (int j = 0;j<19;j++) {
+				if(GO_BOARD[i][j][1]==1) searchSurroundings(i,j,0);
+				if(GO_BOARD[i][j][0]==1) searchSurroundings(i,j,1);
+			}
+		}
+	}
+	
+	public void searchSurroundings(int i, int j, int k) {
+		int required = 4;
+		int stoneSurround = 0;
+		if (i==0) {
+			required--;
+			//check top
+		}
+		else {
+			if(GO_BOARD[i-1][j][k]==1) stoneSurround++;
+		}
+		if (j==0){
+			required--;
+			//check left
+		}
+		else {
+			if(GO_BOARD[i][j-1][k]==1) stoneSurround++;
+		}
+		if (i==18) {
+			required--;
+			//check bottom
+		}
+		else {
+			if(GO_BOARD[i+1][j][k]==1) stoneSurround++;
+		}
+		if (j==18){
+			required--;
+			//check right
+		}
+		else {
+			if(GO_BOARD[i][j+1][k]==1) stoneSurround++;
+		}
+		if (stoneSurround==required) GO_BOARD[i][j][1-k]=0;
+	}
+	
+	
 	public static void main(String[] args) {
 		Board go = new Board();
-		go.makeMove("A19","B");
-		go.makeMove("A15","B");
-		go.makeMove("B14","W");
-		go.makeMove("K2","W");
+		go.makeMove("A19","W");
+		go.makeMove("B19","B");
+		go.makeMove("A18","B");
+		go.makeMove("B18","B");
+		go.printBoard();
+		go.updateGoCaptures();
 		go.printBoard();
 	}
 }
