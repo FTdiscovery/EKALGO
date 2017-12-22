9c662fc44e1674c57fcdc8399ff7a2b0e7e241ea
FTdiscovery <gordonchi11@gmail.com>
2017-12-22 07:44:46 -0500

    Create Library.java

---
+++ Library.java
@@ -0,0 +1,14 @@
+package ekalGO;
+
+/*
+ * This is a library of games that ekalGO will be able to access. This will also be the games that are given to the computer.
+ * The files will be read via BufferedReader. A document folder will have to be created...
+ * 
+ * Admittedly, this class is rather meta (it's a library for the library). 
+ * 
+ * Nonetheless, it will play the games and connect each state with the next action.
+ */
+
+public class Library {
+
+}
