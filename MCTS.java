e016f45521cf4bd86092b2b7d187c234a1cc68f1
FTdiscovery <gordonchi11@gmail.com>
2017-12-22 07:44:33 -0500

    Create MCTS.java

---
+++ MCTS.java
@@ -0,0 +1,11 @@
+package ekalGO;
+
+/*
+ * This is a class that goes through the most basic functions and algorithms required for the Monte Carlo Tree Search. 
+ * 
+ * EkalGO uses this to make decisions for familiar states after training. The values can be used in an Alpha-Beta method, and
+ * this library MCTS has a guideline for it. However, it may not be needed.
+ */
+public class MCTS {
+
+}
